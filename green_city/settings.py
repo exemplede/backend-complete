7dcbce7db299a85a6e6f0b385df0c3db364265ce
Yugiro Hanma <ouedraogojeanclaude566039@gmail.com>
2026-03-09 12:32:28 +0100

    Modification settings

--- green_city/settings.py
+++ green_city/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'dev-only-change-me-ok'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -86,7 +86,5 @@ REST_FRAMEWORK = {
 }
 
 # CORS Configuration
-CORS_ALLOWED_ORIGINS = [
-   
-]
+CORS_ALLOWED_ORIGINS = True 
 CORS_ALLOW_CREDENTIALS = True
