79567b03ebe25e7feeca39bdab00513440d578f3
Yugiro Hanma <ouedraogojeanclaude566039@gmail.com>
2026-03-09 11:46:02 +0100

    Second commit

--- green_city/settings.py
+++ green_city/settings.py
@@ -87,9 +87,6 @@ REST_FRAMEWORK = {
 
 # CORS Configuration
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:5173",
+   '*',
 ]
 CORS_ALLOW_CREDENTIALS = True
