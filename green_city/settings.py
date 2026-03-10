e40f59479b30d60228816cf381b56f6f2b8ff38b
Yugiro Hanma <ouedraogojeanclaude566039@gmail.com>
2026-03-10 12:33:15 +0100

    Modification settings

--- green_city/settings.py
+++ green_city/settings.py
@@ -97,5 +97,6 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://backend-complete-l3hp.onrender.com",
+    "https://project-tutore.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True
