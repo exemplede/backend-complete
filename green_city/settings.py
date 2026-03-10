ebc7d44b7438daa5a0bb4d0c59dd3554af6c36a2
Yugiro Hanma <ouedraogojeanclaude566039@gmail.com>
2026-03-10 12:09:33 +0100

    Modification de la base de donne

--- green_city/settings.py
+++ green_city/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import dj_database_url
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,12 +53,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'green_city.wsgi.application'
 ASGI_APPLICATION = 'green_city.asgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ['DATABASE_URL'], conn_max_age=600)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
